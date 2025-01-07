d8f13a7a1fa6cfceb4f0cd14b7a129dc4a34e9f8
Maria Silvia <maria.lorraine.silvia4@gmail.com>
2025-01-07 20:49:17 +0000

    add mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,16 @@ const port = 4000;
 
 const indexRouter = require("./routes/index");
 
+// Mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGO_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // Router
 app.use("/", indexRouter);
 // app.use("/user-api", userRouter);
